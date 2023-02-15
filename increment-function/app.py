a68f568bc44ccee566ff5e05ac3f609633dd2b32
Lawrance Huynh <lawranch@uci.edu>
2023-02-15 11:39:50 -0800

    Add files via upload

---
+++ increment-function/app.py
@@ -0,0 +1,23 @@
+import json, boto3
+
+client = boto3.client('dynamodb')
+TableName = 'cloud-resume'
+
+
+def lambda_handler(event, context):
+    response = client.update_item(
+        TableName='cloud-resume',
+        Key={
+            'stat': {'S': 'visitor_count'}
+        },
+        UpdateExpression='ADD quantity :inc',
+        ExpressionAttributeValues={":inc": {"N": "1"}},
+        ReturnValues='UPDATED_NEW'
+    )
+
+    value = response['Attributes']['quantity']['N']
+
+    return {
+        'statusCode': 200,
+        'body': value
+    }
